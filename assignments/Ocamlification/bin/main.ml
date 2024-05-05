fef8e22947b3e22cd79eb5bdcf96aa56d8446a51
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-05-05 10:42:37 +0400

    .

--- assignments/Ocamlification/bin/main.ml
+++ assignments/Ocamlification/bin/main.ml
@@ -1,27 +1,8 @@
-type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
-
-let t = 
-  Node (1, 
-  Node (2, 
-    Node(4, 
-      Node (6, Leaf, Leaf) , 
-      Node (7, Leaf, Leaf)) ,
-    Node(5, Leaf, Leaf)), 
-  Node(3, Leaf, Leaf))
-
-let rec size = function
-| Leaf -> 0
-| Node (_, l , r) -> 1 + size l + size r
-
-let rec sum = function 
-| Leaf -> 0
-| Node (v, l, r) -> v + sum l + sum r
-
-
-let rec map f = function 
-| Leaf -> Leaf
-| Node (v, l, r) -> Node (f v, map f l, map f r)
-
-let rec fold acc f= function
-| Leaf -> acc
-| Node (v, l , r) -> f v (fold acc f l) (fold acc f r)
+let foo x y b = 
+  let rec func x y b = match (x<y) with 
+    | true -> if b then func (x+1) y false else func x (y-1) true
+    | false -> x
+  in 
+  match x>y with 
+  | true -> func y x b
+  | false -> func x y b 
