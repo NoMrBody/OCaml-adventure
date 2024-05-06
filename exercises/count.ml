e34815fabb419d0b1db376885b504c4744b79c40
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-05-06 17:55:45 +0400

    .

--- exercises/count.ml
+++ exercises/count.ml
@@ -1,5 +1,5 @@
 (* function that counts true elements of the list *)
 let rec truzz lst acc = match lst with
 | [] -> acc
-| h::t -> if (h=true) then truzz t (acc+1) else truzz t acc
+| h::t -> if h=true then truzz t (acc+1) else truzz t acc
 
--- exercises/filter_evens.ml
+++ exercises/filter_evens.ml
@@ -3,7 +3,7 @@ let rec filter_evens lst =
   let rec test n = if (n mod 2 = 0) then true else false
 in match lst with 
 | [] -> []
-| h::t -> if (test h) then h::filter_evens t else filter_evens t
+| h::t -> if test h then h::filter_evens t else filter_evens t
 
 
 (* function that returns even numbered elements of the list *)
