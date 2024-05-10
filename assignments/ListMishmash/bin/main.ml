7e228bb6412a6e490ffd19c8797cd58bae0e5caa
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-05-10 14:39:43 +0400

    .

--- assignments/ListMishmash/bin/main.ml
+++ assignments/ListMishmash/bin/main.ml
@@ -1,9 +1,9 @@
 (* Interleave 2 *)
 (* output should look like this [1;2;3], [0;0] -> [1;0;2;0;3] *)
 let rec interleave2 lst1 lst2 = match lst1, lst2 with
-| [], [] -> []
-| h::t, [] | [], h::t -> h::t
-| h1::t1, h2::t2 -> h1::h2::interleave2 t1 t2
+  | [], [] -> []
+  | h::t, [] | [], h::t -> h::t
+  | h1::t1, h2::t2 -> h1::h2::interleave2 t1 t2
 
 (* Interleave 3 *)
 let rec interleave3 lst1 lst2 lst3 = match lst1, lst2, lst3 with
