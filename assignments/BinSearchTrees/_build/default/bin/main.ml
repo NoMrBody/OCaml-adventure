f5968b4653b727c6bb0fe34cb8028309c2608899
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-05-19 22:35:01 +0400

    .

---
+++ assignments/BinSearchTrees/_build/default/bin/main.ml
@@ -0,0 +1,48 @@
+type 'a tree = Leaf | Node of 'a * 'a tree * 'a tree
+
+let rec size tr = match tr with
+  | Leaf -> 0
+  | Node (_, l, r) -> 1 + size l + size r
+
+let rec depth tr = match tr with
+  | Leaf -> 0
+  | Node (_, l, r) -> 1 + max (depth l) (depth r)
+
+let rec insert a tr = match tr with
+  | Leaf -> Node(a, Leaf, Leaf)
+  | Node (x, l, r) -> if a<x then Node(x, insert a l, Leaf) else Node(x, Leaf, insert a r)
+
+let rec min tr = match tr with
+  | Leaf -> failwith "empty"
+  | Node (v, Leaf, _) -> v
+  | Node (_, l, _) -> min l
+
+let rec max1 tr = match tr with
+  | Leaf -> min_int
+  | Node(v, l, r) -> max v (max (max1 l) (max1 r))
+
+let rec traverse tr = match tr with
+  | Leaf -> []
+  | Node(v, l, r) -> traverse l @ [v] :: traverse r
+
+let rec remove e tr = match tr with
+  | Leaf -> Leaf
+  | Node (v, l, r) -> if e<v then Node (v, remove e l, r) else 
+                  if e>v then Node(v, l, remove e r) 
+                  else match l, r with
+                    | Leaf, Leaf -> Leaf
+                    | Leaf, _ -> r
+                    | _, Leaf -> l
+                    | _ -> Node (min r, l, remove min r)  (*when node has 2 kids*)
+
+
+let t = 
+  Node (1, 
+  Node (2, 
+    Node(4, 
+      Node (6, Leaf, Leaf) , 
+      Node (7, Leaf, Leaf)) ,
+    Node(5, Leaf, Leaf)), 
+  Node(3, Leaf, Leaf))
+
+(* Leaf |> insert 5 |> insert 7 |> insert 1;; *)
