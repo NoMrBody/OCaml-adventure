5ad79f518d006b579b007b35c6fe157a27c40897
th3g3ntl3man <kutchukhidze.nik@gmail.com>
2024-06-23 19:58:51 +0400

    .

--- assignments/IndexedOneTwoTree/bin/main.ml
+++ assignments/IndexedOneTwoTree/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+(* let tr = Two (4, 1,
+              Two (2, 2, Two(1,1, 
+                                  Leaf "a",
+                                  Leaf "b"),
+                          Two (1, 1,
+                                  Leaf "c", 
+                                  Leaf "d")
+              ),
+              One (1, Leaf "e"));; *)
